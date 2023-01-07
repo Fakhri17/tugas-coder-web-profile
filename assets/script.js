105ae1b7242615b3f6447e69840c2ce6b089f5f9
Fakhri17 <fakhricoffe@gmail.com>
2023-01-07 15:41:36 +0700

    first commit

---
+++ assets/script.js
@@ -0,0 +1,38 @@
+var lazyLoadInstance = new LazyLoad({
+  // Your custom settings go here
+});
+
+function profile(){
+  return{
+    experience: [
+     {title: 'Vanilla-Js', bg: 'bg-yellow-300'},
+     {title: 'Jquery', bg: 'bg-blue-300'},
+     {title: 'Craft CMS', bg: 'bg-red-300'},
+     {title: 'Boostrap', bg: 'bg-indigo-300'},
+     {title: 'Tailwind', bg: 'bg-green-300'},
+     {title: 'Alpine Js', bg: 'bg-blue-300'},
+     {title: 'Vue Js', bg: 'bg-green-300'},
+     {title: 'Nuxt Js', bg: 'bg-green-300'},
+     {title: 'Mysql', bg: 'bg-gray-300'},
+     {title: 'PHP', bg: 'bg-indigo-300'},
+     {title: 'GIT', bg: 'bg-gray-300'},
+     {title: 'C/C++', bg: 'bg-blue-300'},
+    ],
+   
+    connect(){
+    },
+  }; 
+}
+
+function contact(){
+  return{
+    sosmed: [
+      {title: 'github.com/fakhri17', class: 'bi bi-github', url: 'https://github.com/fakhri17'},
+      {title: '@fakhriii_15', class: 'bi bi-twitter', url: 'https://twitter.com/fakhriii_15'},
+      {title: '@fakhriii_15', class: 'bi bi-instagram', url: 'https://www.instagram.com/fakhriii_15/'},
+    ],
+   
+    connect(){
+    },
+  }; 
+}
